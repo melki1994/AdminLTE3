0f5b689c725ace2ef81fc1a64761ad49059ba0a3
takwa melki <takwamelki484@gmail.com>
2021-08-05 09:22:26 +0100

    admin-dashbord

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import Home from './Components/Home'
+import SideNav from './Components/SideNav'
+import './App.css';
+
+function App() {
+  return (
+    <div >
+            <Header/>
+            <Home/>
+            <SideNav/>
+            <Footer/> 
+    </div>
+  );
+}
+
+export default App;
+
